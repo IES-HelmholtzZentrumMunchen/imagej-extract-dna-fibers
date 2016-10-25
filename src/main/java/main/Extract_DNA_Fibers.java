7ac5bdd838e334a0827b0f266c8b8cd4ca00f870
julien.pontabry <julien.pontabry@helmholtz-muenchen.de>
2016-10-25 10:23:03 +0200

    Parallelize segments building.

--- src/main/java/main/Extract_DNA_Fibers.java
+++ src/main/java/main/Extract_DNA_Fibers.java
@@ -142,76 +142,107 @@ public class Extract_DNA_Fibers implements PlugInFilter {
 		double    maxGap2 = maxGap * maxGap;
 		double minLength2 = minLength * minLength;
 		
-		List<Line> segments = new Vector<Line>();
-		
 		// Setup list of foreground pixels' coordinates in coordinate system with origin centered.
 		ImagePoint origin = ImagePoint.getCenterPointOfImage(binary);
 		List<ImagePoint> foregroundPoints = ImagePoint.getImageForegroundPoints(binary, origin);
-				
-		for (HoughPoint peak : selectedPoints) {
-			// Precompute
-			double cosTheta = Math.cos(peak.theta);
-			double sinTheta = Math.sin(peak.theta);
-			
-			// Keep only associated points with that particular peak and compute range
-			List<ImagePoint> associatedPoints = new Vector<ImagePoint>();
-			int minX = binary.getWidth(), maxX = 0;
-			int minY = binary.getHeight(), maxY = 0;
-			
-			for (ImagePoint p : foregroundPoints) {
-				double rho = p.x * cosTheta + p.y * sinTheta;
-				
-				if (peak.rho-tolerance <= rho && rho <= peak.rho+tolerance) {
-					associatedPoints.add(p);
-					
-					if (p.x < minX)
-						minX = p.x;
-					else if (p.x > maxX)
-						maxX = p.x;
-					
-					if (p.y < minY)
-						minY = p.y;
-					else if (p.y > maxY)
-						maxY = p.y;
-				}
-			}
-			
-			// Sort coordinates by in direction of major coordinates change
-			final int factor = (maxX-minX < maxY-minY) ? -1 : 1;
-			
-			associatedPoints.sort((o1,o2) -> {
-				if (o1.x == o2.x)
-					return factor * (o1.y - o2.y);
-				else // o1.x != o2.x
-					return factor * (o1.x - o2.x);
-			});
-			
-			// Creates list of point indices describing gaps
-			List<Integer> indices = new Vector<Integer>();
-			indices.add(-1);
-			
-			for (int i = 0; i < associatedPoints.size()-1; i++) {
-				double distance = associatedPoints.get(i).squaredDistanceToPoint(associatedPoints.get(i+1));
+		
+		// Setup generation functions to be executed in parallel
+		List<Callable<List<Line>>> tasks = new Vector<>();
 
-				if (distance > maxGap2)
-					indices.add(i);
-			}
-			
-			indices.add(associatedPoints.size()-1);
-			
-			// Accumulate segments
-			for (int i = 0; i < indices.size()-1; i++) {
-				ImagePoint p1 = new ImagePoint(associatedPoints.get(indices.get(i)+1));
-				ImagePoint p2 = new ImagePoint(associatedPoints.get(indices.get(i+1)));
-				
-				if (p1.squaredDistanceToPoint(p2) >= minLength2) {
-					p1.add(origin); p2.add(origin);
-					segments.add(new Line(p1.x, p1.y, p2.x, p2.y));
+		IntStream.range(0, selectedPoints.size()).forEach(k -> {
+			tasks.add(() -> {
+				HoughPoint peak = selectedPoints.get(k);
+						
+				// Precompute
+				double cosTheta = Math.cos(peak.theta);
+				double sinTheta = Math.sin(peak.theta);
+
+				// Keep only associated points with that particular peak and compute range
+				List<ImagePoint> associatedPoints = new Vector<ImagePoint>();
+				int minX = binary.getWidth(), maxX = 0;
+				int minY = binary.getHeight(), maxY = 0;
+
+				for (ImagePoint p : foregroundPoints) {
+					double rho = p.x * cosTheta + p.y * sinTheta;
+
+					if (peak.rho-tolerance <= rho && rho <= peak.rho+tolerance) {
+						associatedPoints.add(p);
+
+						if (p.x < minX)
+							minX = p.x;
+						else if (p.x > maxX)
+							maxX = p.x;
+
+						if (p.y < minY)
+							minY = p.y;
+						else if (p.y > maxY)
+							maxY = p.y;
+					}
 				}
-			}
+
+				// Sort coordinates by in direction of major coordinates change
+				final int factor = (maxX-minX < maxY-minY) ? -1 : 1;
+
+				associatedPoints.sort((o1,o2) -> {
+					if (o1.x == o2.x)
+						return factor * (o1.y - o2.y);
+					else // o1.x != o2.x
+						return factor * (o1.x - o2.x);
+				});
+
+				// Creates list of point indices describing gaps
+				List<Integer> indices = new Vector<Integer>();
+				indices.add(-1);
+
+				for (int i = 0; i < associatedPoints.size()-1; i++) {
+					double distance = associatedPoints.get(i).squaredDistanceToPoint(associatedPoints.get(i+1));
+
+					if (distance > maxGap2)
+						indices.add(i);
+				}
+
+				indices.add(associatedPoints.size()-1);
+
+				// Accumulate segments
+				List<Line> segments = new Vector<Line>();
+				for (int i = 0; i < indices.size()-1; i++) {
+					ImagePoint p1 = new ImagePoint(associatedPoints.get(indices.get(i)+1));
+					ImagePoint p2 = new ImagePoint(associatedPoints.get(indices.get(i+1)));
+
+					if (p1.squaredDistanceToPoint(p2) >= minLength2) {
+						p1.add(origin); p2.add(origin);
+						segments.add(new Line(p1.x, p1.y, p2.x, p2.y));
+					}
+				}
+				
+				return segments;
+			});
+		});
+
+		// Run threads in parallel and reduce results
+		List<Line>   allSegments = new Vector<Line>();
+		ExecutorService executor = Executors.newWorkStealingPool();
+
+		try {
+			executor.invokeAll(tasks)
+			.stream()
+			.map(future -> {
+				try {
+					return future.get();
+				}
+				catch (Exception e) {
+					throw new IllegalStateException(e);
+				}
+			})
+			.forEach(segments -> {
+				allSegments.addAll(segments);
+			});
+		}
+		catch (Exception e) {
+			IJ.error("Exception", "An exception occured!\n" + e.getMessage());
 		}
 		
-		return segments;
+		return allSegments;
 	}
 	
 	/**
